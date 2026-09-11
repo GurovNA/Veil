82aa8d4546457001c5eaf1bedb12b9d9417c45f3
GurovNA <gurovnikita6@gmail.com>
2026-09-11 20:43:44 +0000

    v1.2.2: sync panel VERSION with release tag (fix perpetual update loop)

--- panel.py
+++ panel.py
@@ -14,7 +14,7 @@ XRAY = "/usr/local/etc/xray/config.json"
 TOKEN_FILE = f"{BASE}/github.token"
 TELEMT_API = "http://127.0.0.1:9091"
 REPO = "GurovNA/Veil"
-VERSION = "1.2.0"
+VERSION = "1.2.2"
 SESSIONS = {}
 
 # ---------- helpers ----------
