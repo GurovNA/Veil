b666d8316048193d08ded67ef83d7676ef8aa160
GurovNA <gurovnikita6@gmail.com>
2026-09-12 20:11:51 +0000

    v1.7.5: анимированный логотип на экране входа (пульс-кольцо, floaty), карточка с плавным входом

--- panel.py
+++ panel.py
@@ -14,7 +14,7 @@ XRAY = "/usr/local/etc/xray/config.json"
 TOKEN_FILE = f"{BASE}/github.token"
 TELEMT_API = "http://127.0.0.1:9091"
 REPO = "GurovNA/Veil"
-VERSION = "1.7.4"
+VERSION = "1.7.5"
 _GROUP_ORDER = ("reality", "vless", "vmess", "trojan", "ss")
 _GROUP_LABELS = {"reality": "Reality", "vless": "VLESS", "vmess": "VMess",
                  "trojan": "Trojan", "ss": "Shadowsocks"}
